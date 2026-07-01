2bd18617da5eade1fff9064f808e8e974ec27124
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-07-01 11:47:00 +0100

    Hobby classes and data added

---
+++ Hobby-hub/Data Models/HobbyScores.cs
@@ -0,0 +1,21 @@
+﻿namespace Hobby_hub.Data_Models
+{
+    public class HobbyScores
+    {
+        public int Active { get; set; }
+
+        public int Creative { get; set; }
+
+        public int Relaxing { get; set; }
+
+        public int Social { get; set; }
+
+        public int Outdoor { get; set; }
+
+        public int Strategic { get; set; }
+
+        public int Price { get; set; }
+
+        public int TimeCommitment { get; set; }
+    }
+}

15181e11d54e2d5cff2cf702f76d76edd150f766
Marcus Paterson <marcus_paterson@outlook.com>
2026-07-14 11:39:24 +0100

    feat: created Home route

--- hobby-hub-frontend/src/App.tsx
+++ hobby-hub-frontend/src/App.tsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Footer } from "./components/Footer";
 import { NavBar } from "./components/NavBar";
 import { AboutUs } from "./pages/AboutUs";
+import { Home } from "./pages/Home"; 
 
 function App() {
     return (
       <BrowserRouter>
-        <div className=" min-h-screen flex flex-col justify-between mx-auto p-4" >
-                <NavBar />
+        <div className=" min-h-screen flex flex-col mx-auto p-4" >
+          <NavBar />
                 <Routes>
+                    <Route path="/" element={<Home />} />
                     <Route path="/about-us" element={<AboutUs />} />
                 </Routes>
           <Footer />
-            </div>
+        </div>
 
         
       </BrowserRouter>
--- hobby-hub-frontend/src/components/NavBar.tsx
+++ hobby-hub-frontend/src/components/NavBar.tsx
@@ -4,8 +4,9 @@ import appLogo from "../assets/logo.png";
 export function NavBar() {
   return (
     <nav className="flex items-center justify-between h-24">
-      <img className="object-contain h-24" src={appLogo}></img>
+      <Link to="/"><img className="object-contain h-24" src={appLogo}></img></Link>
       <div className="flex gap-3 items-end">
+        <Link to="/">Home</Link>
         <Link to="/quiz">Quiz</Link>
         <Link to="/about-us">About Us</Link>
         <Link to="/browse-all">Browse All</Link>

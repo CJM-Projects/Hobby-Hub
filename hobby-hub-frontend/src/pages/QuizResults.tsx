c2bdda9c48a2f6127a2f9038406c388b4cb450d4
Marcus Paterson <marcus_paterson@outlook.com>
2026-07-22 15:38:03 +0100

    feat: created QuizResults useEffect and fetch to receive quiz results from backend

--- hobby-hub-frontend/src/pages/QuizResults.tsx
+++ hobby-hub-frontend/src/pages/QuizResults.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
+import type { QuizResults } from "../models/QuizResults";
+import { QuizResultsHobbyCard } from "../components/QuizResultsHobbyCard";
+
+
+
 export function QuizResults() {
+    const [searchParams] = useSearchParams();
+    const [isLoading, setIsLoading] = useState(true);
+    const [quizResults, setQuizResults] = useState<QuizResults>();
+
+    useEffect(() => {
+        fetch(`https://localhost:7203/Quiz/results?${searchParams.toString()}`)
+            .then((response) => response.json())
+            .then((json) => setQuizResults(json))
+            .then(() => setIsLoading(false))
+            .catch((err) => {
+                console.error("Failed to fetch matching hobbies.", err);
+                setIsLoading(false);
+            });
+    }, []);
+
+
     return (
-        <h1>Quiz Results</h1>
+        <>
+            <h1>Quiz Results</h1>
+            <div>
+                {isLoading ? (<p>Loading...</p>) : (
+                        quizResults!.results.map((quizResult) => (
+                            <QuizResultsHobbyCard quizResult={quizResult} />
+                        ))
+                )}
+            </div>
+        </>
     );
 }
+

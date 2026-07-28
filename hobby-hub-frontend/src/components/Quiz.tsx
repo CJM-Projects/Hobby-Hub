c0dee8a50c3e5e467a56f647b1f18b9f73e90190
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-07-28 12:28:57 +0100

    added styling to Quiz component for progress bar

--- hobby-hub-frontend/src/components/Quiz.tsx
+++ hobby-hub-frontend/src/components/Quiz.tsx
@@ -2,13 +2,17 @@ import { useState, useEffect } from "react";
 import { QuizCard } from "./QuizCard";
 
 export function Quiz({ questions } : QuizProp) {
-    const [progress, setProgress] = useState(0);
+    const [progress, setProgress] = useState(70);
     return (
-        <div>
-            <progress role="progressbar" max="100" value={progress}> </progress>
-            <QuizCard question={questions[0].question} image={questions[0].image} answers={questions[0].answers} />
+        <div className="flex items-center gap-10 flex-col m-5">
+        <div className="w-full  h-4 bg-gray-200 rounded-full overflow-hidden">
+            <div className="h-full bg-blue-500 transition-all duration-300" style={{ width: `${progress}%` }}></div>
+            
         </div>
-
+        <div>
+                <QuizCard question={questions[0].question} image={questions[0].image} answers={questions[0].answers} />
+            </div>
+        </div>
     )
 }
 export type AnswerOption = {

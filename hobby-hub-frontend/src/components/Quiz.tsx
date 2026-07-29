75bb4206166d96578fbd672a9f6aedb4cdc3c5a9
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-07-29 15:38:46 +0100

    added prev/next buttons to quiz page

--- hobby-hub-frontend/src/components/Quiz.tsx
+++ hobby-hub-frontend/src/components/Quiz.tsx
@@ -1,16 +1,28 @@
 import { useState, useEffect } from "react";
+
 import { QuizCard } from "./QuizCard";
 
 export function Quiz({ questions } : QuizProp) {
-    const [progress, setProgress] = useState(70);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const progress = ((currentQuestion + 1) / questions.length) * 100;
+
     return (
-        <div className="flex items-center gap-10 flex-col m-5">
-        <div className="w-full  h-4 bg-gray-200 rounded-full overflow-hidden">
-            <div className="h-full bg-blue-500 transition-all duration-300" style={{ width: `${progress}%` }}></div>
-            
-        </div>
-        <div>
-                <QuizCard question={questions[0].question} image={questions[0].image} answers={questions[0].answers} />
+        <div className="flex items-center gap-4 flex-col m-5">
+            <div className="w-full  h-4 bg-gray-200 rounded-full overflow-hidden">
+                <div className="h-full bg-blue-500 transition-all duration-300" style={{ width: `${progress}%` }}></div>
+            </div>
+            <div className="flex w-full justify-between">
+                
+                <button className="max-w-100 text-xl py-2 px-4 text-center font-medium rounded-xl border border-slate-200 text-slate-700 transition-all duration-200 hover:border-indigo-500 hover:bg-indigo-50/50 hover:text-indigo-900 shadow-sm">
+                     ← Prev
+                </button>
+                <button className="max-w-100  text-xl py-2 px-4 text-center font-medium rounded-xl border border-slate-200 bg-white text-slate-700 transition-all duration-200 hover:border-indigo-500 hover:bg-indigo-50/50 hover:text-indigo-900 shadow-sm">
+                    Next → 
+                </button>
+            </div>
+            <div>
+                <QuizCard question={questions[currentQuestion].question} image={questions[currentQuestion].image} answers={questions[currentQuestion].answers} />
             </div>
         </div>
     )

29b3a43f0a390f0e3fbd6e9cde521bbdcb079b1d
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-07-29 15:52:48 +0100

    added functionality to prev/next buttons

--- hobby-hub-frontend/src/components/Quiz.tsx
+++ hobby-hub-frontend/src/components/Quiz.tsx
@@ -5,19 +5,30 @@ import { QuizCard } from "./QuizCard";
 export function Quiz({ questions } : QuizProp) {
     const [currentQuestion, setCurrentQuestion] = useState(0);
 
-    const progress = ((currentQuestion + 1) / questions.length) * 100;
+    const progress = ((currentQuestion) / questions.length) * 100;
+
+    const prevQuestion = () => {
+        if (currentQuestion > 0) {
+            setCurrentQuestion((prevIndex) => prevIndex - 1);
+        }
+    };
+
+    const nextQuestion = () => {
+        if (currentQuestion < questions.length - 1) {
+            setCurrentQuestion((prevIndex) => prevIndex + 1);
+        }
+    };
 
     return (
         <div className="flex items-center gap-4 flex-col m-5">
             <div className="w-full  h-4 bg-gray-200 rounded-full overflow-hidden">
                 <div className="h-full bg-blue-500 transition-all duration-300" style={{ width: `${progress}%` }}></div>
             </div>
-            <div className="flex w-full justify-between">
-                
-                <button className="max-w-100 text-xl py-2 px-4 text-center font-medium rounded-xl border border-slate-200 text-slate-700 transition-all duration-200 hover:border-indigo-500 hover:bg-indigo-50/50 hover:text-indigo-900 shadow-sm">
+            <div className="flex w-full justify-between"> 
+                <button className="max-w-100 text-xl py-2 px-4 text-center font-medium rounded-xl border border-slate-200 text-slate-700 transition-all duration-200 hover:border-indigo-500 hover:bg-indigo-50/50 hover:text-indigo-900 shadow-sm" onClick={prevQuestion}>
                      ← Prev
                 </button>
-                <button className="max-w-100  text-xl py-2 px-4 text-center font-medium rounded-xl border border-slate-200 bg-white text-slate-700 transition-all duration-200 hover:border-indigo-500 hover:bg-indigo-50/50 hover:text-indigo-900 shadow-sm">
+                <button className="max-w-100  text-xl py-2 px-4 text-center font-medium rounded-xl border border-slate-200 bg-white text-slate-700 transition-all duration-200 hover:border-indigo-500 hover:bg-indigo-50/50 hover:text-indigo-900 shadow-sm" onClick={nextQuestion}>
                     Next → 
                 </button>
             </div>

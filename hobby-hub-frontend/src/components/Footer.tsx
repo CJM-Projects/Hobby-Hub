1ce85fee25618aeddcea309eeb09d2e614c678ba
Chris <211069418+chris-mech@users.noreply.github.com>
2026-08-26 16:15:57 +0100

    change back quiz results card styling and edit footer styling

--- hobby-hub-frontend/src/components/Footer.tsx
+++ hobby-hub-frontend/src/components/Footer.tsx
@@ -4,10 +4,13 @@ import linkIcon from "../assets/linkIcon.png";
 export function Footer() {
   return (
     <footer className="bottom-0">
-      <hr></hr>
-      <div className="mx-auto max-w-6xl px-20 py-2">
-        <h2 className="font-bold">Support</h2>
-        <Link className="flex items-center gap-1 text-xs" to="./contact-us">
+      <hr className="border-zinc-300 shadow-sm"></hr>
+      <div className="mx-auto max-w-6xl px-8 py-2">
+        <h2 className="font-bold mb-1.5">Support</h2>
+        <Link
+          className="flex items-center gap-1 text-xs mb-2"
+          to="./contact-us"
+        >
           Contact Us{" "}
           <img
             className="object-contain h-2.5 inline-block"

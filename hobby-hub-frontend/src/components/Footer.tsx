11c5c13382d9e9478d3768cf5d2b8684064d5b8b
Marcus Paterson <marcus_paterson@outlook.com>
2026-08-26 17:07:09 +0100

    edit: Updated location of footer.

--- hobby-hub-frontend/src/components/Footer.tsx
+++ hobby-hub-frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="bottom-0">
       <hr className="border-zinc-300 shadow-sm"></hr>
-      <div className="mx-auto max-w-6xl px-8 py-2">
+      <div className="mx-auto px-12 py-2">
         <h2 className="font-bold mb-1.5">Support</h2>
         <Link
           className="flex items-center gap-1 text-xs mb-2"
